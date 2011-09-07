3abc1fa6ce2b064796f8b4741d5e0c72e4147dc7
Andrew Bennett <andrew@delorum.com>
2011-09-07 13:06:22 -0600

    Compile assets.

--- config/application.rb
+++ config/application.rb
@@ -47,9 +47,6 @@ module Karenandrew
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.compile = true
-    config.assets.digest  = true
-    config.assets.precompile = [ /\w+\..+/ ] # compile everything and the kitchen sink
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
