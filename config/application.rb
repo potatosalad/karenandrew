175405abc50295a549f25d364cb2d6127b52abd5
Andrew Bennett <andrew@delorum.com>
2011-09-07 12:45:33 -0600

    Add everything to precompile assets.

--- config/application.rb
+++ config/application.rb
@@ -47,6 +47,7 @@ module Karenandrew
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile = [ /\w+\..+/ ] # compile everything and the kitchen sink
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
