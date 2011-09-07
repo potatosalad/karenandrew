61b428adcd7f5c22cd183d245abb2400216b6d7c
Andrew Bennett <andrew@delorum.com>
2011-09-07 11:35:47 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karenandrew::Application.config.secret_token = 'd3091b91486f57491e9894e3c43c7534ff11d93ec4360ac0518e6c8a79aea0939cf58464bc0878de0f3a74afe531420eeab556f286ba0586ea1a1b809119ac0a'
